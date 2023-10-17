d114848a9fea14a0dc8a94de0d948492e2a3623d
Nikita Glebskiy <naglebskiy_1@edu.hse.ru>
2023-10-17 16:31:32 +0300

    final bot 3.0

--- main.py
+++ main.py
@@ -1,76 +1,81 @@
 import telebot
+import logging
 
-bot = telebot.TeleBot('6053071242:AAGWljaFwR40j151jcRrxrScLUf32FrpNHU')
+TOKEN = "6053071242:AAGWljaFwR40j151jcRrxrScLUf32FrpNHU"  # Токен вашего бота
+FILENAME = "survey_results.txt"  # Имя файла для сохранения результатов опроса
+
+bot = telebot.TeleBot(TOKEN)
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, 'Добро пожаловать! Пожалуйста, введите свою фамилию и имя:')
-    bot.register_next_step_handler(message, get_name)
+    bot.send_message(message.chat.id, "Привет! Для начала анкетирования нажми кнопку 'Старт' ниже.",
+                     reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=True).row('Старт'))
 
 
-def get_name(message):
-    chat_id = message.chat.id
+@bot.message_handler(func=lambda message: message.text == "Старт")
+def ask_name(message):
+    bot.send_message(message.chat.id, "Введите вашу фамилию и имя:")
+    bot.register_next_step_handler(message, ask_team)
+
+
+def ask_team(message):
     name = message.text
-    bot.send_message(chat_id, f'Привет, {name}! Из какой Вы команды на майноре?')
-    bot.register_next_step_handler(message, get_team)
+    with open(FILENAME, 'a') as file:
+        file.write(f"Имя: {name}\n")
+    bot.send_message(message.chat.id, f"Привет, {name}! Из какой ты команды на майноре?")
+    bot.register_next_step_handler(message, ask_phone)
 
 
-def get_team(message):
-    chat_id = message.chat.id
+def ask_phone(message):
     team = message.text
-    bot.send_message(chat_id, 'Введите свой номер телефона:')
-    bot.register_next_step_handler(message, get_phone)
+    with open(FILENAME, 'a') as file:
+        file.write(f"Команда на майноре: {team}\n")
+    bot.send_message(message.chat.id, "Введите свой номер телефона:")
+    bot.register_next_step_handler(message, ask_services)
 
 
-def get_phone(message):
-    chat_id = message.chat.id
+def ask_services(message):
     phone = message.text
-    bot.send_message(chat_id, 'Какие услуги Вам нужны?', reply_markup=create_keyboard())
-    bot.register_next_step_handler(message, get_service)
+    with open(FILENAME, 'a') as file:
+        file.write(f"Номер телефона: {phone}\n")
+
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add('Создание телеграм бота', 'Создание сайта', 'Что-то другое')
+
+    bot.send_message(message.chat.id, "Какая услуга Вам нужна?", reply_markup=markup)
+    bot.register_next_step_handler(message, process_services)
 
 
-def get_service(message):
-    chat_id = message.chat.id
-    service = message.text
+def process_services(message):
+    services = message.text
+    with open(FILENAME, 'a') as file:
+        file.write(f"Выбранные услуги: {services}\n")
 
-    if service == 'Что-то другое':
-        bot.send_message(chat_id, 'Пожалуйста, опишите, какая именно услуга Вам нужна:')
-        bot.register_next_step_handler(message, get_other_service)
+    if services == 'Что-то другое':
+        bot.send_message(message.chat.id, "Напишите, какая услуга вам нужна:")
+        bot.register_next_step_handler(message, process_other)
     else:
-        save_info(message)
-        bot.send_message(chat_id, 'Спасибо за заполнение анкеты! Мы с Вами свяжемся.')
+        finish_survey(message)
 
 
-def get_other_service(message):
-    chat_id = message.chat.id
+def process_other(message):
     other_service = message.text
-    message.text = other_service + ' (Другое)'
-    save_info(message)
-    bot.send_message(chat_id, 'Спасибо за заполнение анкеты! Мы с Вами свяжемся.')
+    with open(FILENAME, 'a') as file:
+        file.write(f"Другая услуга: {other_service}\n")
+    finish_survey(message)
 
 
-def save_info(message):
-    chat_id = message.chat.id
-    username = message.from_user.username
-    name = message.from_user.first_name + ' ' + message.from_user.last_name
-    team = message.text
-    phone = message.text
-    service = message.text
+def finish_survey(message):
+    bot.send_message(message.chat.id, "Спасибо за заполнение анкеты!")
+    bot.send_message(message.chat.id, "Мы свяжемся с вами в ближайшее время.")
 
-    with open('anketa.txt', 'a') as file:
-        file.write(f'Username: {username}\n')
-        file.write(f'Name: {name}\n')
-        file.write(f'Team: {team}\n')
-        file.write(f'Phone: {phone}\n')
-        file.write(f'Service: {service}\n')
-        file.write('-' * 30 + '\n')
+    # Удаляем клавиатуру после завершения опроса
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(message.chat.id, "Хорошего дня!", reply_markup=telebot.types.ReplyKeyboardRemove())
 
 
-def create_keyboard():
-    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    buttons = ["Создание телеграм бота", "Создание сайта", "Что-то другое"]
-    keyboard.add(*buttons)
-    return keyboard
-
-bot.polling(none_stop=True, interval=0)
+if __name__ == '__main__':
+    bot.polling()
